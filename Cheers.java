1c6bdc2fa5d4330dff4749fc4236fe1df84df83e
lior-yossef <lior.yossef@post.runi.ac.il>
2024-11-11 20:21:14 +0200

    Add Cheers Functionality

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,25 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+                String str = args[0].toUpperCase();
+                int printLimit = Integer.parseInt(args[1]);
+
+                String anChars = "AEFHILMNORSX";
+
+                int index = 0;
+                int printCount = 0;
+
+                while (index < str.length()) {
+                        char character = str.charAt(index);
+                        System.out.println("Give me " + (anChars.indexOf(character) == -1 ? "an " : "a ") + character + ": " + character + "!");
+                        index++;
+                }
+
+                System.out.println("What does that spell?");
+
+                while (printCount < printLimit) {
+                        System.out.println(str + "!!!");
+                        printCount++;
+                }
         }
 }
