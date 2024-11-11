27542a59fcf881ea55b828a75c28cdb747d60339
lior-yossef <lior.yossef@post.runi.ac.il>
2024-11-11 20:47:05 +0200

    Add CalcPi Fanctionality

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,22 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+		int numOfTerms = Integer.parseInt(args[0]);
+
+		double pi = 0;
+		int termsCount = 0;
+		double denominator = 1.0;
+
+		while (termsCount < numOfTerms) {
+			double fraction = 1.0 / denominator;
+			pi = termsCount % 2 == 0 ? pi + fraction : pi - fraction;
+			termsCount++;
+			denominator += 2.0;
+		}
+
+		pi = pi * 4.0;
+
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated: " + pi);
 	}
 }
