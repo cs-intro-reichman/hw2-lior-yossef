7ad6f4a23eeeda7338b7902ba5061413f82694a4
lior-yossef <lior.yossef@post.runi.ac.il>
2024-11-14 19:20:23 +0200

    switch while loop to a for loop

--- CalcPi.java
+++ CalcPi.java
@@ -1,16 +1,14 @@
 // Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
+	public static void main(String[] args) {
 		int numOfTerms = Integer.parseInt(args[0]);
 
 		double pi = 0;
-		int termsCount = 0;
 		double denominator = 1.0;
 
-		while (termsCount < numOfTerms) {
+		for (int i = 0; i < numOfTerms; i++) {
 			double fraction = 1.0 / denominator;
-			pi = termsCount % 2 == 0 ? pi + fraction : pi - fraction;
-			termsCount++;
+			pi = i % 2 == 0 ? pi + fraction : pi - fraction;
 			denominator += 2.0;
 		}
 
